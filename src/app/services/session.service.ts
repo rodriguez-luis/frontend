b403fcb92b881324491b806ef715db1e6c403546
rodriguez-luis <pb.rndm@gmail.com>
2020-12-16 15:51:37 -0400

    add session service

---
+++ src/app/services/session.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SessionService {
+  url: string = 'hhtp://localhost:8080/v1/user/';
+
+  constructor(private http: HttpClient) { }
+
+  getUser(): Observable<User> {
+    return this.http.get<User>(this.url);
+  }
+}
