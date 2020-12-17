36fba2280c3a67ca9221e896354f39f9d2d04b4d
rodriguez-luis <pb.rndm@gmail.com>
2020-12-17 14:27:49 -0400

    update order component

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,15 +1,109 @@
 import { Component, OnInit } from '@angular/core';
-
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import { Observable } from "rxjs";
+import { CartService } from '../services/cart.service';
+import { MatDialog } from '@angular/material/dialog';
+import { OrderService } from '../services/order.service';
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
   styleUrls: ['./order.component.css']
 })
 export class OrderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public formGroup: any;
+  titleAlert: string = "This field is required";
+  post: any = "";
+  total: number = 0;
+  
+  constructor(private formBuilder: FormBuilder, public cartService: CartService, private dialog: MatDialog, private checkoutService: OrderService) {}
+  
+  ngOnInit() {
+    this.createForm();
+    this.setChangeValidate();
+    this.getTotal();
   }
-
+  getTotal(){
+    this.total= this.cartService.getTotal();
+  }
+  createForm() {
+    this.formGroup = this.formBuilder.group({
+      email: [
+        null,
+        [Validators.required, Validators.email],
+        this.checkInUseEmail
+      ],
+      name: [null, Validators.required],
+      password: [null, [Validators.required, this.checkPassword]],
+      description: [
+        null,
+        [Validators.required, Validators.minLength(5), Validators.maxLength(100)]
+      ],
+      validate: ""
+    });
+  }
+  
+  setChangeValidate() {
+    this.formGroup.get("validate").valueChanges.subscribe((validate: any) => {
+      if (validate == "1") {
+        this.formGroup
+          .get("name")
+          .setValidators([Validators.required, Validators.minLength(5)]);
+        this.titleAlert = "You need to specify at least 5 characters";
+      } else {
+        this.formGroup.get("name").setValidators(Validators.required);
+      }
+      this.formGroup.get("name").updateValueAndValidity();
+    });
+  }
+  
+  get name() {
+    return this.formGroup.get("name") as FormControl;
+  }
+  
+  checkPassword(control:any) {
+    let enteredPassword = control.value;
+    let passwordCheck = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/;
+    return !passwordCheck.test(enteredPassword) && enteredPassword
+      ? { requirements: true }
+      : null;
+  }
+  
+  checkInUseEmail(control:any) {
+    // mimic http database access
+    let db = ["jack@torchwood.com"];
+    return new Observable(observer => {
+      setTimeout(() => {
+        let result =
+          db.indexOf(control.value) !== -1 ? { alreadyInUse: true } : null;
+        observer.next(result);
+        observer.complete();
+      }, 4000);
+    });
+  }
+  
+  getErrorEmail() {
+    return this.formGroup.get("email").hasError("required")
+      ? "Field is required"
+      : this.formGroup.get("email").hasError("pattern")
+      ? "Not a valid emailaddress"
+      : this.formGroup.get("email").hasError("alreadyInUse")
+      ? "This emailaddress is already in use"
+      : "";
+  }
+  
+  getErrorPassword() {
+    return this.formGroup.get("password").hasError("required")
+      ? "Field is required (at least eight characters, one uppercase letter and one number)"
+      : this.formGroup.get("password").hasError("requirements")
+      ? "Password needs to be at least eight characters, one uppercase letter and one number"
+      : "";
+  }
+  
+  onSubmit(post: any) {
+    this.checkoutService.post({"cartId":1 ,"paymentDetailsId": 1, "total":this.total, "contact":post.email, "details":post.description}).subscribe(data => {this.post = "Guardado con exito!!!";});
+  
+  }
+  close(){
+    this.dialog.closeAll();
+  }
 }
