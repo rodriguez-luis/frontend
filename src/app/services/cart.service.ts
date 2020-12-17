23e4c06944959ce7803deec5b9252893338ce42e
rodriguez-luis <pb.rndm@gmail.com>
2020-12-17 00:16:42 -0400

    add cart service

---
+++ src/app/services/cart.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Product } from '../model/Product'
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+  products: Product[] = [];
+  total: number = 0;
+  constructor() { }
+
+  addProduct(product: Product){
+    this.products.push(product);
+  }
+  clear(){
+    this.products = [];
+  }
+  getProducts():Product[]{
+    return this.products;
+  }
+  getTotal():number{
+    this.total = 0;
+    for (let product of this.products){
+      this.total+=product.unitPrice;
+      console.log(product.unitPrice);
+    }
+    return this.total;
+  }
+  deleteProduct(product: Product){
+    let temp:Product[]=[];
+    for (let x of this.products){
+      if (x.productId != product.productId){
+        temp.push(x);
+      }
+    }
+    this.products = temp;
+  }
+}
