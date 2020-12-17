e2bb7b64fa89309eb58f7ce9c9c53c8bca72ea94
rodriguez-luis <pb.rndm@gmail.com>
2020-12-17 05:27:34 -0400

    add cart models

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
 import { StoreComponent } from './store/store.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/', pathMatch: 'full'},
   {path: 'store', component: StoreComponent},
+  {path: 'cart', component: CartComponent}
 ];
 
 @NgModule({
